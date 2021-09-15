832e786ab8a170425bd95a984b7b52b3c53c08a9
Martins924 <secretariovirtual65@gmail.com>
2021-09-15 10:23:24 -0300

    tudo certo e nada errado

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title> Cadastro Basicao </title>
+</head>
+<body>
+<form method="POST" action="receberDados.php">
+<label>Nome:</label><input type="text" name="nome" id="login"><br>
+<label>Telefone:</label><input type="text" name="telefone" id="senha"><br>
+<input type="submit" value="Cadastrar" id="cadastrar" name="cadastrar">
+</form>
+</body>
+</html>
